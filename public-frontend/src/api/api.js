3311cff949fc233785b9c6900591b37dc6837cab
AbhishekCNC <abhishek_dev@codencoach.com>
2025-12-25 20:43:33 +0530

    link update01

--- public-frontend/src/api/api.js
+++ public-frontend/src/api/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://127.0.0.1:5000";
+export const API_BASE_URL = "https://hello-dewas-web.onrender.com";
 export const API_URL = `${API_BASE_URL}/api`;
 
 export const getLatestNews = async () => {
